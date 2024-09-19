5aa0684ee40e61449eafe9e70d44ffe2dd8fe4be
Satheesh <59468802+shakthi2704@users.noreply.github.com>
2024-09-19 21:12:56 +0530

    Initial commit

---
+++ code/index.js
@@ -0,0 +1,14 @@
+// let age = 25
+// let price = 10.25
+// let gpa = 2.5
+
+// console.log(`your are ${age} years old`)
+// console.log(`the price is ${price}`)
+
+// console.log(typeof age)
+
+// let students = 30
+// students = students++
+// console.log(students)
+
+// ------------------------------------------------------------
